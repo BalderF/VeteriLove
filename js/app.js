b74feb0c04c0bdb27c6e819249d5d12df5a0679c
BalderF <131628970+BalderF@users.noreply.github.com>
2024-07-02 20:47:31 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@ const menu = document.querySelector('.hamburguesa');
 const navegacion = document.querySelector('.navegacion');
 const imagenes = document.querySelectorAll('img');
 const btnTodos = document.querySelector('.todos');
-const btnPlatos = document.querySelector('.platos');
-const btnCorreas = document.querySelector('.correas');
-const btnJuguetes = document.querySelector('.juguetes');
-const btnCamas = document.querySelector('.camas');
-const contenedorProductos = document.querySelector('.productos');
+const btnEnsaladas = document.querySelector('.ensaladas');
+const btnPasta = document.querySelector('.pasta');
+const btnPizza = document.querySelector('.pizza');
+const btnPostres = document.querySelector('.postres');
+const contenedorPlatillos = document.querySelector('.platillos');
 document.addEventListener('DOMContentLoaded',()=>{
     eventos();
-    productos();
+    platillos();
 });
 
 const eventos = () =>{
@@ -69,43 +69,43 @@ const cerrarMenu = (boton, overlay) =>{
     }
 }
 
-const productos = () =>{
-    let productosArreglo = [];
-    const productos = document.querySelectorAll('.producto');
+const platillos = () =>{
+    let platillosArreglo = [];
+    const platillos = document.querySelectorAll('.platillo');
 
-    productos.forEach(producto=> productosArreglo = [...productosArreglo,producto]);
+    platillos.forEach(platillo=> platillosArreglo = [...platillosArreglo,platillo]);
 
-    const platos = productosArreglo.filter(ensalada=> ensalada.getAttribute('data-producto') === 'plato');
-    const correas = productosArreglo.filter(correa => correa.getAttribute('data-producto') === 'correa');
-    const juguetes = productosArreglo.filter(juguete => juguete.getAttribute('data-producto') === 'juguete');
-    const camas = productosArreglo.filter(cama=> cama.getAttribute('data-producto') === 'cama');
+    const ensaladas = platillosArreglo.filter(ensalada=> ensalada.getAttribute('data-platillo') === 'ensalada');
+    const pastas = platillosArreglo.filter(pasta => pasta.getAttribute('data-platillo') === 'pasta');
+    const pizzas = platillosArreglo.filter(pizza => pizza.getAttribute('data-platillo') === 'pizza');
+    const postres = platillosArreglo.filter(postre=> postre.getAttribute('data-platillo') === 'postre');
 
-    mostrarProductos(platos, correas, juguetes, camas, productosArreglo);
+    mostrarPlatillos(ensaladas, pastas, pizzas, postres, platillosArreglo);
 
 }
 
-const mostrarProductos = (platos, correas, juguetes, camas, todos) =>{
-    btnPlatos.addEventListener('click', ()=>{
-        limpiarHtml(contenedorProductos);
-        platos.forEach(ensalada=> contenedorProductos.appendChild(ensalada));
+const mostrarPlatillos = (ensaladas, pastas, pizzas, postres, todos) =>{
+    btnEnsaladas.addEventListener('click', ()=>{
+        limpiarHtml(contenedorPlatillos);
+        ensaladas.forEach(ensalada=> contenedorPlatillos.appendChild(ensalada));
     });
 
-    btnCorreas.addEventListener('click', ()=>{
-        limpiarHtml(contenedorProductos);
-        correas.forEach(correa=> contenedorProductos.appendChild(correa));
+    btnPasta.addEventListener('click', ()=>{
+        limpiarHtml(contenedorPlatillos);
+         pastas.forEach(pasta=> contenedorPlatillos.appendChild(pasta));
     });
 
-    btnJuguetes.addEventListener('click', ()=>{
-        limpiarHtml(contenedorProductos);
-        juguetes.forEach(juguete=> contenedorProductos.appendChild(juguete));
+    btnPizza.addEventListener('click', ()=>{
+        limpiarHtml(contenedorPlatillos);
+        pizzas.forEach(pizza=> contenedorPlatillos.appendChild(pizza));
     });
-    btnCamas.addEventListener('click', ()=>{
-        limpiarHtml(contenedorProductos);
-        camas.forEach(cama=> contenedorProductos.appendChild(cama));
+    btnPostres.addEventListener('click', ()=>{
+        limpiarHtml(contenedorPlatillos);
+        postres.forEach(postre=> contenedorPlatillos.appendChild(postre));
     });
     btnTodos.addEventListener('click',()=>{
-        limpiarHtml(contenedorProductos);
-        todos.forEach(todo=> contenedorProductos.appendChild(todo));
+        limpiarHtml(contenedorPlatillos);
+        todos.forEach(todo=> contenedorPlatillos.appendChild(todo));
     });
 }
 
